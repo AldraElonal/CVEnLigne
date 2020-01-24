68bf1ad30052629e885de5890c958c47ea1f7dd7
AldraElonal <46710857+AldraElonal@users.noreply.github.com>
2020-01-24 21:10:21 +0100

    utilisation bootstrap pour design basé sur les cards ajout contenu condensé partie formation ajout formulaire de contact

--- index.php
+++ index.php
@@ -51,85 +51,95 @@
 </nav>
 
 <div class="container">
-    <h2>Biographie</h2>
-    <div class="row">
+    <div class="card">
+        <h2 class="card-header">Biographie</h2>
+        <div class="row card-body">
 
-        <img src="https://picsum.photos/200/300" alt="photo" class="photo col-md-5">
-        <div class="col-md-7"
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quam tortor, ullamcorper tristique vehicula vel,
-            molestie non nisi. Nam sollicitudin nunc at elementum varius. Mauris dui tortor, semper at orci ut, lacinia
-            sollicitudin velit. Morbi turpis nisl, vestibulum sit amet neque sit amet, lacinia cursus massa. Etiam in
-            dictum justo. Praesent gravida lacinia quam, id porta dui commodo et. Donec id finibus mauris, id
-            condimentum odio. Cras sit amet leo quam. Nam in velit suscipit ipsum eleifend hendrerit. Suspendisse a
-            interdum diam.</p>
+            <img src="https://picsum.photos/200/300" alt="photo" class="photo col-md-5">
+            <div class="col-md-7"
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quam tortor, ullamcorper tristique vehicula
+                vel,
+                molestie non nisi. Nam sollicitudin nunc at elementum varius. Mauris dui tortor, semper at orci ut,
+                lacinia
+                sollicitudin velit. Morbi turpis nisl, vestibulum sit amet neque sit amet, lacinia cursus massa. Etiam
+                in
+                dictum justo. Praesent gravida lacinia quam, id porta dui commodo et. Donec id finibus mauris, id
+                condimentum odio. Cras sit amet leo quam. Nam in velit suscipit ipsum eleifend hendrerit. Suspendisse a
+                interdum diam.</p>
 
-        <p>Suspendisse suscipit odio non egestas placerat. Proin elementum leo vel sapien lacinia pulvinar. Suspendisse
-            eget ullamcorper quam. Duis tempus velit imperdiet odio tempor cursus. Nunc interdum congue libero, nec
-            finibus diam consectetur non. Phasellus non mattis augue, nec fringilla elit. Suspendisse porta nibh risus,
-            id accumsan ex ullamcorper ut. Sed tempus suscipit libero, vel suscipit nulla tincidunt at. Suspendisse vel
-            elementum massa. Etiam a urna porta, egestas libero eu, gravida risus. Pellentesque at ex mauris.
-            Suspendisse rutrum ligula eget vestibulum pellentesque. Fusce rhoncus vitae arcu in ultricies. </p>
+            <p>Suspendisse suscipit odio non egestas placerat. Proin elementum leo vel sapien lacinia pulvinar.
+                Suspendisse
+                eget ullamcorper quam. Duis tempus velit imperdiet odio tempor cursus. Nunc interdum congue libero, nec
+                finibus diam consectetur non. Phasellus non mattis augue, nec fringilla elit. Suspendisse porta nibh
+                risus,
+                id accumsan ex ullamcorper ut. Sed tempus suscipit libero, vel suscipit nulla tincidunt at. Suspendisse
+                vel
+                elementum massa. Etiam a urna porta, egestas libero eu, gravida risus. Pellentesque at ex mauris.
+                Suspendisse rutrum ligula eget vestibulum pellentesque. Fusce rhoncus vitae arcu in ultricies. </p>
+        </div>
     </div>
 </div>
 
-<section id="Competences">
-    <h2> Compétences</h2>
-    <h3>Languages & Logiciels</h3>
-    <div class="row">
-        <figure>
-            <img src="https://picsum.photos/150/150" alt="logo language">
-            <figcaption>PHP</figcaption>
-        </figure>
-        <figure>
-            <img src="https://picsum.photos/150/150" alt="logo language">
-            <figcaption>HTML</figcaption>
-        </figure>
-        <figure>
-            <img src="https://picsum.photos/150/150" alt="logo language">
-            <figcaption>CSS</figcaption>
-        </figure>
-        <figure>
-            <img src="https://picsum.photos/150/150" alt="logo language">
-            <figcaption>Javacript</figcaption>
-        </figure>
+<section id="Competences" class="card">
+    <h2 class="card-header"> Compétences</h2>
+    <div class="card-body">
+        <h3>Languages & Logiciels</h3>
+        <div class="row">
+            <figure>
+                <img src="https://picsum.photos/150/150" alt="logo language">
+                <figcaption>PHP</figcaption>
+            </figure>
+            <figure>
+                <img src="https://picsum.photos/150/150" alt="logo language">
+                <figcaption>HTML</figcaption>
+            </figure>
+            <figure>
+                <img src="https://picsum.photos/150/150" alt="logo language">
+                <figcaption>CSS</figcaption>
+            </figure>
+            <figure>
+                <img src="https://picsum.photos/150/150" alt="logo language">
+                <figcaption>Javacript</figcaption>
+            </figure>
+        </div>
+        <div class="row">
+            <figure>
+                <img src="https://picsum.photos/150/150" alt="logo language">
+                <figcaption>JQuery</figcaption>
+            </figure>
+            <figure>
+                <img src="https://picsum.photos/150/150" alt="logo language">
+                <figcaption>Symfony</figcaption>
+            </figure>
+            <figure>
+                <img src="https://picsum.photos/150/150" alt="logo language">
+                <figcaption>Git</figcaption>
+            </figure>
+            <figure>
+                <img src="https://picsum.photos/150/150" alt="logo language">
+                <figcaption>SQL</figcaption>
+            </figure>
+        </div>
+        <h3>Savoir Faire</h3>
+        <ul>
+            <li>Reporting</li>
+            <li>Diagnostic de pannes</li>
+            <li>Gestion de Projet</li>
+            <li>Développement avec outils de gestion de version</li>
+            <li>Respect du cahier des charges</li>
+        </ul>
+        <h3>Savoir Etre</h3>
+        <ul>
+            <li>Autonomie</li>
+            <li>Esprit d'initiative</li>
+            <li>Travail d'équipe</li>
+        </ul>
     </div>
-    <div class="row">
-        <figure>
-            <img src="https://picsum.photos/150/150" alt="logo language">
-            <figcaption>JQuery</figcaption>
-        </figure>
-        <figure>
-            <img src="https://picsum.photos/150/150" alt="logo language">
-            <figcaption>Symfony</figcaption>
-        </figure>
-        <figure>
-            <img src="https://picsum.photos/150/150" alt="logo language">
-            <figcaption>Git</figcaption>
-        </figure>
-        <figure>
-            <img src="https://picsum.photos/150/150" alt="logo language">
-            <figcaption>SQL</figcaption>
-        </figure>
-    </div>
-    <h3>Savoir Faire</h3>
-    <ul>
-        <li>Reporting</li>
-        <li>Diagnostic de pannes</li>
-        <li>Gestion de Projet</li>
-        <li>Développement avec outils de gestion de version</li>
-        <li>Respect du cahier des charges</li>
-    </ul>
-    <h3>Savoir Etre</h3>
-    <ul>
-        <li>Autonomie</li>
-        <li>Esprit d'initiative</li>
-        <li>Travail d'équipe</li>
-    </ul>
 </section>
 
-<section id="experiences">
-    <h2 id="experience">Expériences</h2>
-    <div>
+<section id="experience" class="card">
+    <h2 class="card-header">Expériences</h2>
+    <div class="card-body">
         <ul>
             <li>
                 <h3>Technicien</h3>
@@ -163,38 +173,44 @@
     </div>
 </section>
 
-<section id="formations">
-    <h2>Formations</h2>
-    <div>
-        <h3>Developpeur Web RNCP niveau 2</h3>
-        <p>Developpement Web : php , javascript, Html/Css, Base de données . .. </p>
-    </div>
-    <div>
-        <h3>Niveau Master Génie des Sytèmes pour l'Aéronautique et les Transport</h3>
-        <p>Developpement Web : php , javascript, Html/Css, Base de données . .. </p>
-    </div>
-
-    <div>
-        <h3>Licence Génie des Sytèmes pour l'Aéronautique et les Transport</h3>
-        <p>Developpement Web : php , javascript, Html/Css, Base de données . .. </p>
-    </div>
-
-
-    <div>
-        <h3>Niveau Ingénieur Génie Electrique et Système de Commande</h3>
-        <p>Developpement Web : php , javascript, Html/Css, Base de données . .. </p>
-    </div>
-
-
-    <div>
-        <h3>DUT Génie Electrique et Informatique Industrielle</h3>
-        <p>Developpement Web : php , javascript, Html/Css, Base de données . .. </p>
+<section id="formations" class="card">
+    <h2 class="card-header">Formations</h2>
+    <div class="card-body">
+        <div>
+            <h3>Developpeur Web RNCP niveau 2</h3>
+            <p>Developpement Web : PHP , Symfony.Javascript,JQuery. HTML/CSS, Base de données avec SQL et
+                PHPMyAdmin.Initiation au SEO Réalisation de divers projet avec soutenance pour valider les
+                compétences. </p>
+        </div>
+        <hr>
+        <div>
+            <h3>Niveau Master Génie des Sytèmes pour l'Aéronautique et les Transport</h3>
+            <p>Culture Aéronautique. Etude du fonctionnement des capteurs, systèmes hydrauliques. Fonctionnement d'un
+                Turboréacteur.Management et Gestion de Projet. </p>
+        </div>
+        <hr>
+        <div>
+            <h3>Licence Génie des Sytèmes pour l'Aéronautique et les Transport</h3>
+            <p>Culture Aéronautique, et découverte du fonctionnement global d'un avion. Initiation à
+                l'Aéorodynamisme. </p>
+        </div>
+        <hr>
+        <div>
+            <h3>Niveau Ingénieur Génie Electrique et Système de Commande</h3>
+            <p>Continuitée du DUT : Electronique, Electrotechnique.Automatisme, Grafcet. Labview, VHDL.Management et
+                Gestion de projet.</p>
+        </div>
+        <hr>
+        <div>
+            <h3>DUT Génie Electrique et Informatique Industrielle</h3>
+            <p>Electronique, Electrotechnique. Automatisme, Grafcet. Programmation en C.</p>
+        </div>
     </div>
 </section>
 
-<section id="portfolio">
-    <h2 ">Portfolio</h2>
-    <div class="row">
+<section id="portfolio" class="card">
+    <h2 class="card-header">Portfolio</h2>
+    <div class="row card-body">
         <figure>
             <img src="https://picsum.photos/150/150" alt="logo language">
             <figcaption>Page Statique</figcaption>
@@ -211,21 +227,33 @@
             <img src="https://picsum.photos/150/150" alt="logo language">
             <figcaption>blog écrivain PHP</figcaption>
         </figure>
-        <figure>
-            <img src="https://picsum.photos/150/150" alt="logo language">
-            <figcaption></figcaption>
-        </figure>
+        Plusieurs images avec animation JS pour affichage des détails. Lien direct vers le projet dispo que sur les détails
     </div>
-    Plusieurs images avec animation JS pour affichage des détails. Lien direct vers le projet dispo que sur les détails
 </section>
 
 
-<section id="contact">
-    <h2>Contact</h2>
-    <div class="row">
-        Formulaire de contact pour envoi mail auto
-        email : rem.leger@laposte.net
-        tel : 06.78.47.86.43
+<section id="contact" class="card">
+    <h2 class="card-header">Contact</h2>
+    <div class="card-body">
+        <form>
+            <div class="form-group">
+                <label for="name">Votre nom: </label>
+                <input type="text" name="name" id="name" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Votre email: </label>
+                <input type="text" name="email" id="email" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="content">Votre Message: </label>
+                <textarea name="content" id="content"  class="form-control" required></textarea>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Envoyer!" class="btn btn-success">
+            </div>
+        </form>
+        <p class="font-italic font-weight-light">email : rem.leger@laposte.net</p>
+        <p class="font-italic font-weight-light">tel : 06.78.47.86.43</p>
     </div>
 </section>
 
