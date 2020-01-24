fd82efe4f8577f41f3821da54a5ff7ddc6c27f4e
AldraElonal <46710857+AldraElonal@users.noreply.github.com>
2020-01-24 17:34:11 +0100

    Projet Site CV en ligne  Commit initial

---
+++ index.php
@@ -0,0 +1,243 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Mon Cv En ligne">
+    <meta name="author" content="Remi LEGER">
+    <title>LEGER Rémi CV en Ligne</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>
+
+
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="style.css"/>
+
+</head>
+<body>
+<div id="top"></div>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="#top">Acceuil</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#Competences">Compétences <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#experience">Expérience</a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#formations">Formations</a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#contact" tabindex="-1" aria-disabled="true">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+    <h2>Biographie</h2>
+    <div class="row">
+
+        <img src="https://picsum.photos/200/300" alt="photo" class="photo col-md-5">
+        <div class="col-md-7"
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quam tortor, ullamcorper tristique vehicula vel,
+            molestie non nisi. Nam sollicitudin nunc at elementum varius. Mauris dui tortor, semper at orci ut, lacinia
+            sollicitudin velit. Morbi turpis nisl, vestibulum sit amet neque sit amet, lacinia cursus massa. Etiam in
+            dictum justo. Praesent gravida lacinia quam, id porta dui commodo et. Donec id finibus mauris, id
+            condimentum odio. Cras sit amet leo quam. Nam in velit suscipit ipsum eleifend hendrerit. Suspendisse a
+            interdum diam.</p>
+
+        <p>Suspendisse suscipit odio non egestas placerat. Proin elementum leo vel sapien lacinia pulvinar. Suspendisse
+            eget ullamcorper quam. Duis tempus velit imperdiet odio tempor cursus. Nunc interdum congue libero, nec
+            finibus diam consectetur non. Phasellus non mattis augue, nec fringilla elit. Suspendisse porta nibh risus,
+            id accumsan ex ullamcorper ut. Sed tempus suscipit libero, vel suscipit nulla tincidunt at. Suspendisse vel
+            elementum massa. Etiam a urna porta, egestas libero eu, gravida risus. Pellentesque at ex mauris.
+            Suspendisse rutrum ligula eget vestibulum pellentesque. Fusce rhoncus vitae arcu in ultricies. </p>
+    </div>
+</div>
+
+<section id="Competences">
+    <h2> Compétences</h2>
+    <h3>Languages & Logiciels</h3>
+    <div class="row">
+        <figure>
+            <img src="https://picsum.photos/150/150" alt="logo language">
+            <figcaption>PHP</figcaption>
+        </figure>
+        <figure>
+            <img src="https://picsum.photos/150/150" alt="logo language">
+            <figcaption>HTML</figcaption>
+        </figure>
+        <figure>
+            <img src="https://picsum.photos/150/150" alt="logo language">
+            <figcaption>CSS</figcaption>
+        </figure>
+        <figure>
+            <img src="https://picsum.photos/150/150" alt="logo language">
+            <figcaption>Javacript</figcaption>
+        </figure>
+    </div>
+    <div class="row">
+        <figure>
+            <img src="https://picsum.photos/150/150" alt="logo language">
+            <figcaption>JQuery</figcaption>
+        </figure>
+        <figure>
+            <img src="https://picsum.photos/150/150" alt="logo language">
+            <figcaption>Symfony</figcaption>
+        </figure>
+        <figure>
+            <img src="https://picsum.photos/150/150" alt="logo language">
+            <figcaption>Git</figcaption>
+        </figure>
+        <figure>
+            <img src="https://picsum.photos/150/150" alt="logo language">
+            <figcaption>SQL</figcaption>
+        </figure>
+    </div>
+    <h3>Savoir Faire</h3>
+    <ul>
+        <li>Reporting</li>
+        <li>Diagnostic de pannes</li>
+        <li>Gestion de Projet</li>
+        <li>Développement avec outils de gestion de version</li>
+        <li>Respect du cahier des charges</li>
+    </ul>
+    <h3>Savoir Etre</h3>
+    <ul>
+        <li>Autonomie</li>
+        <li>Esprit d'initiative</li>
+        <li>Travail d'équipe</li>
+    </ul>
+</section>
+
+<section id="experiences">
+    <h2 id="experience">Expériences</h2>
+    <div>
+        <ul>
+            <li>
+                <h3>Technicien</h3>
+                <div class="row">
+                    <p>Description</p>
+                    <img src="https://picsum.photos/100/100" alt="logo entreprise" class="logoEntreprise">
+                </div>
+            </li>
+            <li>
+                <h3>Ingénieur Logiciel Systeme</h3>
+                <div class="row">
+                    <p>Description</p>
+                    <img src="https://picsum.photos/100/100" alt="logo entreprise" class="logoEntreprise">
+                </div>
+            </li>
+            <li>
+                <h3>Apprenti Ingénieur</h3>
+                <div class="row">
+                    <p>Description</p>
+                    <img src="https://picsum.photos/100/100" alt="logo entreprise" class="logoEntreprise">
+                </div>
+            </li>
+            <li>
+                <h3>Assistant Ingénieur</h3>
+                <div class="row">
+                    <p>Description</p>
+                    <img src="https://picsum.photos/100/100" alt="logo entreprise" class="logoEntreprise">
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
+
+<section id="formations">
+    <h2>Formations</h2>
+    <div>
+        <h3>Developpeur Web RNCP niveau 2</h3>
+        <p>Developpement Web : php , javascript, Html/Css, Base de données . .. </p>
+    </div>
+    <div>
+        <h3>Niveau Master Génie des Sytèmes pour l'Aéronautique et les Transport</h3>
+        <p>Developpement Web : php , javascript, Html/Css, Base de données . .. </p>
+    </div>
+
+    <div>
+        <h3>Licence Génie des Sytèmes pour l'Aéronautique et les Transport</h3>
+        <p>Developpement Web : php , javascript, Html/Css, Base de données . .. </p>
+    </div>
+
+
+    <div>
+        <h3>Niveau Ingénieur Génie Electrique et Système de Commande</h3>
+        <p>Developpement Web : php , javascript, Html/Css, Base de données . .. </p>
+    </div>
+
+
+    <div>
+        <h3>DUT Génie Electrique et Informatique Industrielle</h3>
+        <p>Developpement Web : php , javascript, Html/Css, Base de données . .. </p>
+    </div>
+</section>
+
+<section id="portfolio">
+    <h2 ">Portfolio</h2>
+    <div class="row">
+        <figure>
+            <img src="https://picsum.photos/150/150" alt="logo language">
+            <figcaption>Page Statique</figcaption>
+        </figure>
+        <figure>
+            <img src="https://picsum.photos/150/150" alt="logo language">
+            <figcaption>Wordpress</figcaption>
+        </figure>
+        <figure>
+            <img src="https://picsum.photos/150/150" alt="logo language">
+            <figcaption>Location vélo Toulouse</figcaption>
+        </figure>
+        <figure>
+            <img src="https://picsum.photos/150/150" alt="logo language">
+            <figcaption>blog écrivain PHP</figcaption>
+        </figure>
+        <figure>
+            <img src="https://picsum.photos/150/150" alt="logo language">
+            <figcaption></figcaption>
+        </figure>
+    </div>
+    Plusieurs images avec animation JS pour affichage des détails. Lien direct vers le projet dispo que sur les détails
+</section>
+
+
+<section id="contact">
+    <h2>Contact</h2>
+    <div class="row">
+        Formulaire de contact pour envoi mail auto
+        email : rem.leger@laposte.net
+        tel : 06.78.47.86.43
+    </div>
+</section>
+
+
+</body>
+<!-- JQuery for bootstrap -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script>
+<script src="/docs/4.4/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
+        crossorigin="anonymous"></script>
+</body>
+</html>
